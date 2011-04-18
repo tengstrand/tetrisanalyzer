ea08e694215809433331739abdf1b4357a0a01e0
unknown <joakim.tengstrand@.jaybis.local>
2011-04-18 08:18:12 +0200

    Implemented JavaPieceGenerator (migrated from Scala).

---
+++ src/test/java/com/github/tetrisanalyzer/piecegenerator/JavaPieceGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.github.tetrisanalyzer.piecegenerator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class JavaPieceGeneratorTest {
+
+    @Test
+    public void nextPiece() {
+        PieceGenerator pieceGenerator = new JavaPieceGenerator();
+
+        for (int i=1; i<=1000; i++) {
+            int pieceNumber = pieceGenerator.nextPiece().number();
+
+            assertTrue(pieceNumber >= 1);
+            assertTrue(pieceNumber <= 7);
+        }
+    }
+}
