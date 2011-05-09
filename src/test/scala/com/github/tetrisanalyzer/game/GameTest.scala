fee73f426b239a8fbe140cf6ec0f9f8df4cf4006
unknown <joakim.tengstrand@.jaybis.local>
2011-05-09 19:08:51 +0200

    Updated performance information.

--- src/test/scala/com/github/tetrisanalyzer/game/GameTest.scala
+++ src/test/scala/com/github/tetrisanalyzer/game/GameTest.scala
@@ -60,23 +60,23 @@ class GameTest extends BaseTest {
     timer2.printResult
   }
 */
-  // Performance before optimization (code on http://sourceforge.net/projects/tetrisai:
+  // Performance before optimization (code at http://sourceforge.net/projects/tetrisai:
   // 1 000 000 pieces, sliding on  = 491 sec = 2 000 pieces/sec (274 sec = 56% board evaluation, 44% other)
   // 1 000 000 pieces, sliding off = 451 sec = 2 200 pieces/sec (329 sec = 72% board evaluation, 39% other)
 
-  // Performance before optimization, with optimized TengstrandBoardEvaluator1 (code on http://sourceforge.net/projects/tetrisai:
+  // Performance before optimization, with optimized TengstrandBoardEvaluator1 (code at http://sourceforge.net/projects/tetrisai:
   // 1 000 000 pieces, sliding on  = 396 sec = 2 500 pieces/sec (220 sec = 55% board evaluation, 45% other)
   // 1 000 000 pieces, sliding off = 338 sec = 2 900 pieces/sec (217 sec = 64% board evaluation, 39% other)
 
-  // Performance before optimizations of TetrisBoardEvaluator1 (code on https://github.com/tengstrand/tetrisanalyzer)
+  // Performance before optimizations of TetrisBoardEvaluator1 (code at https://github.com/tengstrand/tetrisanalyzer)
   // 1 000 000 pieces, sliding on  = 355 sec =  2 800 pieces/sec (132 sec = 37% board evaluation, 63% other)
-  // 1 000 000 pieces, sliding off =  92 sec = 10 800 pieces/sec (72 sec = 78% board evaluation, 22% other)
+  // 1 000 000 pieces, sliding off =  163 sec = 6 100 pieces/sec (128 sec = 78% board evaluation, 22% other)
 
-  // Performance after optimizations of TetrisBoardEvaluator1 (code on https://github.com/tengstrand/tetrisanalyzer)
+  // Performance after optimizations of TetrisBoardEvaluator1 (code at https://github.com/tengstrand/tetrisanalyzer)
   // 1 000 000 pieces, sliding on  = 270 sec =  3 700 pieces/sec (44 sec = 16% board evaluation, 84% other)
   // 1 000 000 pieces, sliding off =  68 sec = 14 700 pieces/sec (35 sec = 51% board evaluation, 49% other)
 
-  // Performance Java version (code on https://github.com/tengstrand/tetrisanalyzer))
+  // Performance Java version (code at https://github.com/tengstrand/tetrisanalyzer))
   // 1 000 000 pieces, sliding on =  200 sec =  5 000 pieces/sec (29 sec = 14% board evaluation, 49% other)
   // 1 000 000 pieces, sliding off =  63 sec = 15 800 pieces/sec (24 sec = 38% board evaluation, 62% other)
 }
