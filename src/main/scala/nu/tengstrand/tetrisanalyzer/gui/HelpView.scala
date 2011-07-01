ffa2e943b234051440c2bc45670ee7ae7834f961
unknown <joakim.tengstrand@.jaybis.local>
2011-07-01 22:48:37 +0200

    Changed to [Ctrl] left/right(/up/down.

--- src/main/scala/nu/tengstrand/tetrisanalyzer/gui/HelpView.scala
+++ src/main/scala/nu/tengstrand/tetrisanalyzer/gui/HelpView.scala
@@ -10,28 +10,26 @@ class HelpView extends TextDrawer {
 
     prepareDraw(origoX, g)
 
-    drawText("P                Toggle pause", 1, g)
+    drawText("P               Toggle pause", 1, g)
 
-    drawText("down             Animate next piece (if paused)", 3, g)
+    drawText("down            Animate next piece (if paused)", 3, g)
 
-    drawText("left/right       Increase/decrease speed", 5, g)
+    drawText("left/right      Change speed", 5, g)
 
-    drawText("[Ctrl] + down    Increase board height", 7, g)
-    drawText("[Ctrl] + up      Decrease board height", 8, g)
-    drawText("[Ctrl] + left    Decrease board width", 9, g)
-    drawText("[Ctrl] + right   Increase board width", 10, g)
+    drawText("[Ctrl] + left/  Change board size", 7, g)
+    drawText("right/up/down", 8, g)
 
-    drawText("e / E            Increase/decrease seed", 12, g)
+    drawText("e / E           Change seed", 10, g)
 
-    drawText("l                Toggle sliding", 14, g)
+    drawText("l               Toggle sliding", 12, g)
 
 
 
 
-    drawText("[F1]             Show game info", 18, g)
+    drawText("[F1]            Show game info", 18, g)
 
-    drawText("Tetris Analyzer " + version + " - by Joakim Tengstrand", 20, g)
-    drawText("https://github.com/tengstrand/tetrisanalyzer", 21, g)
-    drawText("Mail: tetrisanalyzer@tengstrand.nu", 22, g)
+    drawText("Tetris Analyzer " + version, 20, g)
+    drawText("tetrisanalyzer@tengstrand.nu", 21, g)
+    drawText("https://github.com/tengstrand/tetrisanalyzer", 22, g)
   }
 }
