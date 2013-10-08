b8518ea8409bf6daa7eb93bc5204307aa91d8557
tengstrand <joakimtengstrand@gmail.com>
2013-10-08 08:17:06 +0200

    Added function write-board!.

--- clojure/src/tetrisanalyzer/file.clj
+++ clojure/src/tetrisanalyzer/file.clj
@@ -1,5 +1,13 @@
-(ns tetrisanalyzer.file)
+(ns tetrisanalyzer.file
+  (:require [tetrisanalyzer.board :refer :all]))
 
 (defn write-file! [filename string]
-  (with-open [w (clojure.java.io/writer filename :append true)]
+  (with-open [w (clojure.java.io/writer filename)]
     (.write w string)))
+
+(defn write-board!
+  ([board] (write-board! board 8))
+  ([board width]
+  (write-file! "C:/TetrisAnalyzer/tetris.txt"
+    (str "\n\n" (board->str board width)))))
+
