0de56a6c4ea4f862554ddac1d914776fcd60a3ad
tengstrand <joakimtengstrand@gmail.com>
2013-10-09 06:40:29 +0200

    Removed unused files.

--- clojure/src/tetrisanalyzer/position.clj
+++
@@ -1,7 +0,0 @@
-(ns tetrisanalyzer.position
-  (:require [tetrisanalyzer.piece :refer :all])
-  (:require [tetrisanalyzer.board :refer :all]))
-
-;; ---------- Manual test ----------
-
-;;(tetrisanalyzer.file/write-file! "C:/Source/IDEA/tetrisanalyzer/clojure/target/tetris.txt" (str newpos))
