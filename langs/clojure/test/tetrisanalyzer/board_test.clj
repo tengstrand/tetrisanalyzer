7fc5eb57aef2a660febe90581e1f4add42e2e93c
tengstrand <joakimtengstrand@gmail.com>
2013-10-09 16:52:06 +0200

    Fixed failing tests.

--- langs/clojure/test/tetrisanalyzer/board_test.clj
+++ langs/clojure/test/tetrisanalyzer/board_test.clj
@@ -22,8 +22,8 @@
 
 (expect [9 0 1 2 3 4 5 6 7 8 0 0 9] (str->row "#-IZSJLTOx--#"))
 
-(expect [21 7 22 7 23 7 32 7]
-  (boardpiece 1 2 7 10 [[0 0][1 0][2 0][1 1]]))
+(expect [21 7 22 7 31 7 32 7]
+  (boardpiece 10 1 2 7 0))
 
 (expect [9 0 0 0 0 0 0 9] (empty-line 8))
 
@@ -40,3 +40,4 @@
 ;;(expect is-piece-free
 
 ;; set-piece-on-board 8 (empty-board) 2 2 6 1
+
