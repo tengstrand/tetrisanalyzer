eae392e9d5e32850214e32c916f2bfbcf1820864
tengstrand <joakimtengstrand@gmail.com>
2014-10-19 08:24:37 +0200

    Minor improvements.

--- langs/java/src/main/java/com/github/tetrisanalyzer/piecegenerator/PredictablePieceGenerator.java
+++ langs/java/src/main/java/com/github/tetrisanalyzer/piecegenerator/PredictablePieceGenerator.java
@@ -6,32 +6,37 @@ import com.github.tetrisanalyzer.settings.PieceSettings;
 import static com.github.tetrisanalyzer.settings.Setting.setting;
 
 public class PredictablePieceGenerator extends PieceGenerator {
-    private int pieceIndex = 0;
+    private int nextPieceIndex;
     private final String pieces;
 
     public PredictablePieceGenerator(PieceSettings settings, String pieces) {
+        this(settings, pieces, 0);
+    }
+
+    public PredictablePieceGenerator(PieceSettings settings, String pieces, int nextPieceIndex) {
         super(settings);
         this.pieces = pieces;
+        this.nextPieceIndex = nextPieceIndex;
     }
 
     @Override
     public PredictablePieceGenerator copy() {
-        return new PredictablePieceGenerator(settings, pieces);
+        return new PredictablePieceGenerator(settings, pieces, nextPieceIndex);
     }
 
     @Override
     public PieceGeneratorSettings settings() {
         return new PieceGeneratorSettings(
-                setting("description", "Generates a predetermined sequence of pieces or piece O"),
+                setting("description", "Predetermined sequence of pieces or 'O' if exceeded"),
                 setting("class", this.getClass().getCanonicalName()),
-                setting("pieceIndex", pieceIndex),
+                setting("next piece index", nextPieceIndex),
                 setting("pieces", pieces));
     }
 
     @Override
     public int nextPieceNumber() {
-        if (pieceIndex < pieces.length()) {
-            return Piece.indexOf(pieces.charAt(pieceIndex++));
+        if (nextPieceIndex < pieces.length()) {
+            return Piece.indexOf(pieces.charAt(nextPieceIndex++));
         }
         return Piece.O;
     }
