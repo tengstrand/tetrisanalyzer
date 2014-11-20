943b1a4008bbe0d7a3123d9057745e780f89147d
tengstrand <gitetris66>
2014-11-20 15:01:41 +0100

    Implemented clipHorizontal(...).

--- langs/java/src/test/java/com/github/tetrisanalyzer/gui/VerticesTest.java
+++ langs/java/src/test/java/com/github/tetrisanalyzer/gui/VerticesTest.java
@@ -6,7 +6,14 @@ import static junit.framework.Assert.assertEquals;
 
 public class VerticesTest {
 
-    @Test
+	private Vertices vertices = new Vertices(
+			new Vertex(0.0, 0.417),
+			new Vertex(1.0, 0.833),
+			new Vertex(2.0, 1.0),
+			new Vertex(3.0, 0.583));
+
+
+	@Test
     public void normalizeY() {
         Vertices vertices = new Vertices(
                 new Vertex(0.0, 500.0),
@@ -24,4 +31,52 @@ public class VerticesTest {
 
         assertEquals(expected, normalized);
     }
+
+	@Test
+	public void clipHorizontal_onBothSides_withLinesBetween() {
+		Vertices result = vertices.clipHorizontal(0.4, 0.9);
+
+		Vertices expected = new Vertices(
+				new Vertex(1.2000000000000002, 0.8664),
+				new Vertex(2.0, 1.0),
+				new Vertex(2.7, 0.7081));
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void clipHorizontal_onBothSides_oneLine() {
+		Vertices result = vertices.clipHorizontal(0.2, 0.22);
+
+		Vertices expected = new Vertices(
+				new Vertex(0.6000000000000001, 0.6666),
+				new Vertex(0.66, 0.69156));
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void clipHorizontal_onlyLeftSide() {
+		Vertices result = vertices.clipHorizontal(0.4, 1);
+
+		Vertices expected = new Vertices(
+				new Vertex(1.2000000000000002, 0.8664),
+				new Vertex(2.0, 1.0),
+				new Vertex(3.0, 0.583));
+
+		assertEquals(expected, result);
+	}
+
+	@Test
+	public void clipHorizontal_onlyRightSide() {
+		Vertices result = vertices.clipHorizontal(0, 0.9);
+
+		Vertices expected = new Vertices(
+				new Vertex(0.0, 0.417),
+				new Vertex(1.0, 0.833),
+				new Vertex(2.0, 1.0),
+				new Vertex(2.7, 0.7081));
+
+		assertEquals(expected, result);
+	}
 }
