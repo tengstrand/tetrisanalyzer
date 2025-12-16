034af70f1a4f783ab67cce6ab09a5c53bf3b7c1c
Joakim Tengstrand <joakimtengstrand@gmai.com>
2025-12-16 12:58:17 +0100

    Remove unused code.

--- langs/clojure/tetris-polylith/components/board/src/tetrisanalyzer/board/interface.clj
+++ langs/clojure/tetris-polylith/components/board/src/tetrisanalyzer/board/interface.clj
@@ -1,12 +1,2 @@
 (ns tetrisanalyzer.board.interface
-  (:require [tetrisanalyzer.board.board :as board]
-            [tetrisanalyzer.board.text-board :as text-board]))
-
-(defn empty-board [width height]
-  (board/empty-board width height))
-
-(defn text-board->board [text-board]
-  (text-board/text-board->board text-board))
-
-(defn board->text-board [board]
-  (text-board/board->text-board board))
+  (:require [tetrisanalyzer.board.board :as board]))
--- langs/clojure/tetris-polylith/components/piece/src/tetrisanalyzer/piece/interface.clj
+++ langs/clojure/tetris-polylith/components/piece/src/tetrisanalyzer/piece/interface.clj
@@ -1,6 +1,5 @@
 (ns tetrisanalyzer.piece.interface
-  (:require [clojure.set :as set]
-            [tetrisanalyzer.piece.piece :as piece]))
+  (:require [tetrisanalyzer.piece.piece :as piece]))
 
 (def I 1)
 (def Z 2)
@@ -9,12 +8,6 @@
 (def L 5)
 (def T 6)
 (def O 7)
-
-(def char->piece-index
-  {\- 0, \I 1, \Z 2, \S 3, \J 4, \L 5, \T 6, \O 7})
-
-(def piece-index->char
-  (set/map-invert char->piece-index))
 
 (defn piece [p rotation]
   (get-in piece/pieces [p rotation]))
