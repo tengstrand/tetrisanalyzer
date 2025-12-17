46f80917ff70305deef130bb0bf76d6bde653698
Joakim Tengstrand <joakimtengstrand@gmail.com>
2025-12-17 08:53:47 +0100

    Remove piece dummy test.

--- langs/clojure/tetris-polylith/components/piece/test/tetrisanalyzer/piece/interface_test.clj
+++
@@ -1,6 +0,0 @@
-(ns tetrisanalyzer.piece.interface-test
-  (:require [clojure.test :as test :refer :all]
-            [tetrisanalyzer.piece.interface :as piece]))
-
-(deftest dummy-test
-  (is (= 1 1)))
