3174f76250240ea47d84e47bfc2f3defd7413864
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-08 20:42:54 +0100

    Put expected first

--- langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/board/test_grid.py
+++ langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/board/test_grid.py
@@ -2,16 +2,16 @@ from tetrisanalyzer.board.grid import board, row
 
 
 def test_grid_row_to_row():
-    assert row("--x--xx-") == [0, 0, 1, 0, 0, 1, 1, 0]
+    assert [0, 0, 1, 0, 0, 1, 1, 0] == row("--x--xx-")
 
 
 def test_grid_to_board():
-    assert board(
+    assert [
+        [0, 0, 1, 0, 0, 1, 1, 0],
+        [1, 0, 0, 0, 0, 0, 0, 1],
+    ] == board(
         [
             "--x--xx-",
             "x------x",
         ]
-    ) == [
-        [0, 0, 1, 0, 0, 1, 1, 0],
-        [1, 0, 0, 0, 0, 0, 0, 1],
-    ]
+    )
