3253560245fb51df8f6bcab6dd68dc24b85578eb
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-08 20:45:56 +0100

    Improve formatting

--- langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/piece/test_shape.py
+++ langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/piece/test_shape.py
@@ -2,4 +2,10 @@ from tetrisanalyzer.piece import shape
 
 
 def test_converts_a_piece_shape_grid_to_a_list_of_xy_cells():
-    assert [[2, 0], [1, 1], [2, 1], [1, 2]] == shape.shape(["--x-", "-xx-", "-x--", "----"])
+    assert [[2, 0], 
+            [1, 1], 
+            [2, 1], 
+            [1, 2]] == shape.shape(["--x-", 
+                                    "-xx-",
+                                    "-x--",
+                                    "----"])
