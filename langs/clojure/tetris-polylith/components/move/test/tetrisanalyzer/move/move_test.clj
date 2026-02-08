868a3609903627306d794db6a4909a3f40568bb5
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-08 15:31:14 +0100

    Test left/right/down movements

--- langs/clojure/tetris-polylith/components/move/test/tetrisanalyzer/move/move_test.clj
+++ langs/clojure/tetris-polylith/components/move/test/tetrisanalyzer/move/move_test.clj
@@ -10,6 +10,7 @@
 (def rotation 0)
 (def S piece/S)
 (def shapes atari-arcade/shapes)
+(def bitmask (piece/rotation-bitmask shapes S))
 (def piece (piece/piece S rotation shapes))
 
 (def board (board/board ['xxxxxxxx
@@ -21,18 +22,38 @@
   (is (= true
          (move/valid-move? board x y S rotation shapes))))
 
-(deftest invalid-move1
-  (is (= false
-         (move/valid-move? board (inc x) y S rotation shapes))))
+(deftest valid-left-move
+  (is (= [2 1 0]
+         (move/left board (inc x) y S rotation nil shapes))))
 
-(deftest invalid-move2
-  (is (= false
-         (move/valid-move? board x (dec y) S rotation shapes))))
+(deftest invalid-left-move
+  (is (= nil
+         (move/left board x y S rotation nil shapes))))
 
-(deftest valid-move3
-  (is (= false
-         (move/valid-move? board x y S (inc rotation) shapes))))
+(deftest valid-right-move
+  (is (= [2 1 0]
+         (move/right board (dec x) y S rotation nil shapes))))
 
-(deftest invalid-move2-outside-board
+(deftest invalid-right-move
+  (is (= nil
+         (move/right board x (dec y) S rotation nil shapes))))
+
+(deftest unoccupied-down-move
+  (is (= [[2 1 0] nil]
+         (move/down board x (dec y) S rotation nil shapes))))
+
+(deftest down-move-hits-ground
+  (is (= [nil [[2 1 0]]]
+         (move/down board x y S rotation nil shapes))))
+
+(deftest valid-rotation
+  (is (= [2 1 0]
+         (move/rotate board x y S (dec rotation) bitmask shapes))))
+
+(deftest invalid-rotation
+  (is (= nil
+         (move/rotate board x y S rotation bitmask shapes))))
+
+(deftest invalid-move-outside-board
   (is (= false
          (move/valid-move? board 10 -10 S rotation shapes))))
