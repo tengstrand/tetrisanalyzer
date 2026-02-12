cf0d8fdb8afc5a2d9f264cf7b6323696804971c8
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-12 19:23:40 +0100

    Adjust formatting

--- langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/piece/test_shape.py
+++ langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/piece/test_shape.py
@@ -5,9 +5,8 @@ def test_converts_a_piece_shape_grid_to_a_list_of_xy_cells():
     assert [[2, 0],
             [1, 1],
             [2, 1],
-            [1, 2]] == shape(
-        ["--x-",
-         "-xx-",
-         "-x--",
-         "----"]
+            [1, 2]] == shape(["--x-",
+                              "-xx-",
+                              "-x--",
+                              "----"]
     )
