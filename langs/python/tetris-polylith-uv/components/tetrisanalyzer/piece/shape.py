c8c9d8dae83a570e262d1f759c701f1b891e316c
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-14 16:55:42 +0100

    formatting

--- langs/python/tetris-polylith-uv/components/tetrisanalyzer/piece/shape.py
+++ langs/python/tetris-polylith-uv/components/tetrisanalyzer/piece/shape.py
@@ -3,11 +3,9 @@ def shape(piece_grid):
         [x, y]
         for y, row in enumerate(piece_grid)
         for x, ch in enumerate(row)
-        if ch == "x"
-    ]
+        if ch == "x"]
 
 def shapes(pieces_grids):
     return [
         [shape(piece_grid) for piece_grid in piece_grids]
-        for piece_grids in pieces_grids
-    ]
+        for piece_grids in pieces_grids]
