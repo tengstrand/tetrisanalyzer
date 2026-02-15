969aef3506109e684025c907d8f025f4624fda89
Joakim Tengstrand <joakimtengstrand@gmail.com>
2026-02-15 14:53:56 +0100

    move test from board to piece

--- langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/board/test_core.py
+++ langs/python/tetris-polylith-uv/test/components/tetrisanalyzer/board/test_core.py
@@ -16,38 +16,8 @@ empty_board = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
 def test_empty_board():
     assert empty_board == board.empty_board(10, 15)
-
-
-def test_set_piece():
-    T = piece.T
-    rotate_two_times = 2
-    piece_t = piece.piece(T, rotate_two_times, atari_arcade.shapes)
-    x = 5
-    y = 13
-    expected = [
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, T, 0, 0, 0],
-        [0, 0, 0, 0, 0, T, T, T, 0, 0],
-    ]
-
-    test_board = [row[:] for row in empty_board]
-    piece.set_piece(test_board, T, x, y, piece_t)
-    assert expected == test_board
